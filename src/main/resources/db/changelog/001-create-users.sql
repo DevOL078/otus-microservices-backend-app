b9726b5f83608a26e59f3d0a71169142bfd72875
DevOL078 <oleinik.dima098@mail.ru>
2024-05-16 07:35:25 +0300

    DB connection

---
+++ src/main/resources/db/changelog/001-create-users.sql
@@ -0,0 +1,8 @@
+create table users (
+    id uuid primary key,
+    username varchar(255) not null,
+    first_name varchar(255),
+    last_name varchar(255),
+    email varchar(255),
+    phone varchar(255)
+);
